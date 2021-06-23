1ba6d8f7db13542d524f2e895e8256fbcb05f0f3
rriyaldhi <rriyaldhi@gmail.com>
2021-06-23 10:34:47 +0200

    Refactor retinaface

--- retinaface.cpp
+++ retinaface.cpp
@@ -120,3 +120,28 @@ cv::Mat RetinaFace::preprocess(cv::Mat &img, int input_w, int input_h) {
     return out;
 }
 
+cv::Rect RetinaFace::get_rect_adapt_landmark(cv::Mat& img, int input_w, int input_h, float bbox[4], float lmk[10]) {
+    int l, r, t, b;
+    float r_w = input_w / (img.cols * 1.0);
+    float r_h = input_h / (img.rows * 1.0);
+    if (r_h > r_w) {
+        l = bbox[0] / r_w;
+        r = bbox[2] / r_w;
+        t = (bbox[1] - (input_h - r_w * img.rows) / 2) / r_w;
+        b = (bbox[3] - (input_h - r_w * img.rows) / 2) / r_w;
+        for (int i = 0; i < 10; i += 2) {
+            lmk[i] /= r_w;
+            lmk[i + 1] = (lmk[i + 1] - (input_h - r_w * img.rows) / 2) / r_w;
+        }
+    } else {
+        l = (bbox[0] - (input_w - r_h * img.cols) / 2) / r_h;
+        r = (bbox[2] - (input_w - r_h * img.cols) / 2) / r_h;
+        t = bbox[1] / r_h;
+        b = bbox[3] / r_h;
+        for (int i = 0; i < 10; i += 2) {
+            lmk[i] = (lmk[i] - (input_w - r_h * img.cols) / 2) / r_h;
+            lmk[i + 1] /= r_h;
+        }
+    }
+    return cv::Rect(l, t, r-l, b-t);
+}
