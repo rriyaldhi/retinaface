57f0af155d793847d27194d605ca075c7ef7b5dd
rriyaldhi <rriyaldhi@gmail.com>
2021-06-29 09:24:44 +0200

    Test inference

--- inference.cpp
+++ inference.cpp
@@ -11,12 +11,12 @@ int main(int argc, char** argv) {
         value.assign(imageBgr.data, imageBgr.data + imageBgr.total() * imageBgr.channels());
     }
     cv::cvtColor(imageBgr, imageRgb, cv::COLOR_BGR2RGB);
-    std::vector<cv::Rect> rectangles = retinaFace.infer(imageRgb, 360, 246);
+//    std::vector<cv::Rect> rectangles = retinaFace.infer(imageRgb, 360, 246);
 
-    for (cv::Rect rectangle: rectangles)
-    {
-        std::cout << rectangle.x << " " << rectangle.y << " " << rectangle.width << " " << rectangle.height << std::endl;
-    }
+//    for (cv::Rect rectangle: rectangles)
+//    {
+//        std::cout << rectangle.x << " " << rectangle.y << " " << rectangle.width << " " << rectangle.height << std::endl;
+//    }
 
     return 0;
 }
