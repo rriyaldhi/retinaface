2471f2377e2b4897a560554bf5e11735ee260f79
rriyaldhi <rriyaldhi@gmail.com>
2021-07-07 11:14:15 +0200

    Test Inference

--- inference.cpp
+++ inference.cpp
@@ -14,12 +14,18 @@ int main(int argc, char** argv) {
     }
 
     cv::Size s = imageRgb.size();
-    std::vector<cv::Rect> rectangles = retinaFace.infer(value, s.width, s.height);
-    std::cout << rectangles.size() << std::endl;
-    for (cv::Rect rectangle: rectangles)
-    {
-        std::cout << rectangle.x << " " << rectangle.y << " " << rectangle.width << " " << rectangle.height << std::endl;
+    int total = 0;
+    int n = 1000;
+    for (int i = 0; i < n; i++) {
+        auto start = std::chrono::system_clock::now();
+        std::vector<cv::Rect> rectangles = retinaFace.infer(value, s.width, s.height);
+        auto end = std::chrono::system_clock::now();
+        if (i > 0)
+            total += std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
     }
+    std::vector<cv::Rect> rectangles = retinaFace.infer(value, s.width, s.height);
+    std::cout << "Detected faces: " << rectangles.size() << std::endl;
+    std::cout << "Total time: " << total / (n - 1) << " us" << std::endl;
 
     return 0;
 }
