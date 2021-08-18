6316ae0167b50ef841d0c002142fd28a0cae32d1
rriyaldhi <rriyaldhi@gmail.com>
2021-08-18 12:32:11 +0200

    test

--- retinaface.cpp
+++ retinaface.cpp
@@ -111,37 +111,39 @@ void RetinaFace::inferVideo(std::string input_video, std::string output_video) {
         std::cout << "ERROR: Failed to write the video" << std::endl;
     }
     if (videoCapture.isOpened()) {
-        std::cout << "Reading..." << std::endl;
-        cv::Mat imageRgb;
-        videoCapture >> imageRgb;
-        cv::Mat imageBgr;
-        cv::cvtColor(imageRgb, imageBgr, cv::COLOR_RGB2BGR);
-        cv::Mat pr_img = RetinaFace::preprocess(imageBgr, RetinaFace::INPUT_W, RetinaFace::INPUT_H);
-        static float data[3 * RetinaFace::INPUT_H * RetinaFace::INPUT_W];
-        float *p_data = &data[0];
-        for (int i = 0; i < RetinaFace::INPUT_H * RetinaFace::INPUT_W; i++) {
-            p_data[i] = pr_img.at<cv::Vec3b>(i)[0] - 104.0;
-            p_data[i + RetinaFace::INPUT_H * RetinaFace::INPUT_W] = pr_img.at<cv::Vec3b>(i)[1] - 117.0;
-            p_data[i + 2 * RetinaFace::INPUT_H * RetinaFace::INPUT_W] = pr_img.at<cv::Vec3b>(i)[2] - 123.0;
-        }
-        static float prob[RetinaFace::OUTPUT_SIZE];
-        RetinaFace::doInference(this->context, data, prob, 1);
+        for (int i = 0; i < 10; i++) {
+                std::cout << "Reading frame: " << i << std::endl;
+                cv::Mat imageRgb;
+                videoCapture >> imageRgb;
+                cv::Mat imageBgr;
+                cv::cvtColor(imageRgb, imageBgr, cv::COLOR_RGB2BGR);
+                cv::Mat pr_img = RetinaFace::preprocess(imageBgr, RetinaFace::INPUT_W, RetinaFace::INPUT_H);
+                static float data[3 * RetinaFace::INPUT_H * RetinaFace::INPUT_W];
+                float *p_data = &data[0];
+                for (int i = 0; i < RetinaFace::INPUT_H * RetinaFace::INPUT_W; i++) {
+                    p_data[i] = pr_img.at<cv::Vec3b>(i)[0] - 104.0;
+                    p_data[i + RetinaFace::INPUT_H * RetinaFace::INPUT_W] = pr_img.at<cv::Vec3b>(i)[1] - 117.0;
+                    p_data[i + 2 * RetinaFace::INPUT_H * RetinaFace::INPUT_W] = pr_img.at<cv::Vec3b>(i)[2] - 123.0;
+                }
+                static float prob[RetinaFace::OUTPUT_SIZE];
+                RetinaFace::doInference(this->context, data, prob, 1);
 
-        std::vector<decodeplugin::Detection> res;
-        RetinaFace::nms(res, &prob[0], IOU_THRESH);
-        cv::Mat tmp = imageBgr.clone();
-        std::vector<cv::Rect> rectangles;
-        for (size_t j = 0; j < res.size(); j++) {
-            if (res[j].class_confidence < CONF_THRESH) continue;
-            cv::Rect rectangle = RetinaFace::getRectangles(tmp, RetinaFace::INPUT_W, RetinaFace::INPUT_H, res[j].bbox, res[j].landmark);
-            rectangles.push_back(rectangle);
-            cv::rectangle(tmp, rectangle, cv::Scalar(0x27, 0xC1, 0x36), 2);
-            for (int k = 0; k < 10; k += 2) {
-                cv::circle(tmp, cv::Point(res[j].landmark[k], res[j].landmark[k + 1]), 1, cv::Scalar(255 * (k > 2), 255 * (k > 0 && k < 8), 255 * (k < 6)), 4);
-            }
+                std::vector<decodeplugin::Detection> res;
+                RetinaFace::nms(res, &prob[0], IOU_THRESH);
+                cv::Mat tmp = imageBgr.clone();
+                std::vector<cv::Rect> rectangles;
+                for (size_t j = 0; j < res.size(); j++) {
+                    if (res[j].class_confidence < CONF_THRESH) continue;
+                    cv::Rect rectangle = RetinaFace::getRectangles(tmp, RetinaFace::INPUT_W, RetinaFace::INPUT_H, res[j].bbox, res[j].landmark);
+                    rectangles.push_back(rectangle);
+                    cv::rectangle(tmp, rectangle, cv::Scalar(0x27, 0xC1, 0x36), 2);
+                    for (int k = 0; k < 10; k += 2) {
+                        cv::circle(tmp, cv::Point(res[j].landmark[k], res[j].landmark[k + 1]), 1, cv::Scalar(255 * (k > 2), 255 * (k > 0 && k < 8), 255 * (k < 6)), 4);
+                    }
+                }
+                cv::cvtColor(tmp, imageRgb, cv::COLOR_RGB2BGR);
+                cv::imwrite("result_" + i + ".jpg", imageRgb);
         }
-        cv::cvtColor(tmp, imageRgb, cv::COLOR_RGB2BGR);
-        cv::imwrite("result.jpg", imageRgb);
     }
 //        cv::Mat imageRgb;
 //        std::cout << "Reading..." << std::endl;
