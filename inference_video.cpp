4e73ecf7dc0b67dc521f09dd8fad233a49d8259a
rriyaldhi <rriyaldhi@gmail.com>
2021-08-18 12:00:31 +0200

    fix inference_video

--- inference_video.cpp
+++ inference_video.cpp
@@ -4,6 +4,6 @@
 
 int main(int argc, char** argv) {
     RetinaFace retinaFace = RetinaFace();
-    std::vector<cv::Rect> rectangles = retinaFace.infer(std::string(argv[1]), std::string(argv[2]));
+    std::vector<cv::Rect> rectangles = retinaFace.inferVideo(std::string(argv[1]), std::string(argv[2]));
     return 0;
 }
